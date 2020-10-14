e4b541d578d187502715a99699fff7b9ef652115
andre <andreantoine0016@gmail.com>
2020-10-14 10:40:17 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAhmBCv4QISEYGFNQEBBwb-4wvig08Fqgs",
+    authDomain: "tech-zone-andre.firebaseapp.com",
+    databaseURL: "https://tech-zone-andre.firebaseio.com",
+    projectId: "tech-zone-andre",
+    storageBucket: "tech-zone-andre.appspot.com",
+    messagingSenderId: "430644037204",
+    appId: "1:430644037204:web:808f84fad5178e58d84fc1",
+    measurementId: "G-Z0HH535E4H"
+  };
